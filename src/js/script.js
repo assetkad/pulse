6c95c50da7cc8883173c18391891a3aa610d18c8
Asset <asset_kad@mail.ru>
2021-08-31 03:09:58 +0500

    step 3 my cat crashed my nootebook damn

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,15 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('.carousel__inner').slick({
         speed: 1200,
         // adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png"></button>',
-        responsive: [
-            {
-                breakpoint: 992,
-                settings: {
-                    dots: true,
-                    arrows: false
-                }
+        responsive: [{
+            breakpoint: 992,
+            settings: {
+                dots: true,
+                arrows: false
             }
-        ]
+        }]
     });
 });
